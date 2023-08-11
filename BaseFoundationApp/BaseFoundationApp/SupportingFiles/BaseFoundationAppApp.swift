a908af10897f5b39cbef7d114bce6fa1fa4599f1
Pablo Bórquez Urquidy <pablo.borquez.urquidy@gmail.com>
2023-08-11 11:13:34 -0700

    Xcode project added

---
+++ BaseFoundationApp/BaseFoundationApp/SupportingFiles/BaseFoundationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseFoundationAppApp.swift
+//  BaseFoundationApp
+//
+//  Created by pablo borquez on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BaseFoundationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
