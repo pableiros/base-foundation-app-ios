19453ef77e8503ac27a0e5ce0bdc66e82b5f2dca
Pablo Bórquez Urquidy <pablo.borquez.urquidy@gmail.com>
2023-08-31 18:20:07 -0700

    Second panel added

--- OnThisDay/OnThisDay/Views/TableView.swift
+++ OnThisDay/OnThisDay/Views/TableView.swift
@@ -9,20 +9,43 @@ import SwiftUI
 
 struct TableView: View {
     @State private var sortOrder = [KeyPathComparator(\Event.year)]
+    @State private var selectedEventID: UUID?
     
     var tableData: [Event]
     var sortedTableData: [Event] {
         return self.tableData.sorted(using: self.sortOrder)
     }
     
+    var selectedEvent: Event? {
+        guard let selectedEventID = self.selectedEventID else { return nil }
+        
+        return self.tableData.first {
+            $0.id == selectedEventID
+        }
+    }
+    
     var body: some View {
-        Table(self.sortedTableData, sortOrder: self.$sortOrder) {
-            TableColumn("Year", value: \.year) {
-                Text($0.year)
+        HStack {
+            Table(self.sortedTableData,
+                  selection: self.$selectedEventID,
+                  sortOrder: self.$sortOrder) {
+                TableColumn("Year", value: \.year) {
+                    Text($0.year)
+                }
+                .width(min: 50, ideal: 60, max: 100)
+                
+                TableColumn("Title", value: \.text)
             }
-            .width(min: 50, ideal: 60, max: 100)
             
-            TableColumn("Title", value: \.text)
+            if let selectedEvent {
+                EventView(event: selectedEvent)
+                    .frame(width: 250)
+            } else {
+                Text("Select an event for more details…")
+                    .font(.title3)
+                    .padding()
+                    .frame(width: 250)
+            }
         }
     }
 }
