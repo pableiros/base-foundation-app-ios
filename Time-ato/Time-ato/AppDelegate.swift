f20636919cf021ceb21e90eecbb62bf33c523f8e
Pablo Bórquez Urquidy <pablo.borquez.urquidy@gmail.com>
2023-09-11 16:41:35 -0700

    Time-ato project created

---
+++ Time-ato/Time-ato/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  Time-ato
+//
+//  Created by pablo borquez on 10/09/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+    
+    @IBOutlet weak var statusMenu: NSMenu!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        self.statusItem?.menu = self.statusMenu
+        
+        self.statusItem?.button?.title = "Time-ato"
+        self.statusItem?.button?.imagePosition = .imageLeading
+        self.statusItem?.button?.image = NSImage(systemSymbolName: "timer",
+                                                 accessibilityDescription: "Time-ato")
+        
+        self.statusItem?.button?.font = NSFont.monospacedSystemFont(ofSize: NSFont.systemFontSize,
+                                                                    weight: .regular)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+}
+
