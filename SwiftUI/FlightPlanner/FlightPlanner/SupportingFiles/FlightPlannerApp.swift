60adafac699d019461a71c11a87e2f613d965bfe
Pablo Bórquez Urquidy <pablo.borquez.urquidy@gmail.com>
2023-10-05 15:22:42 -0700

    FlightPlanner new project added

---
+++ SwiftUI/FlightPlanner/FlightPlanner/SupportingFiles/FlightPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightPlannerApp.swift
+//  FlightPlanner
+//
+//  Created by pablo borquez on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FlightPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/FlightPlanner/FlightPlanner/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlightPlanner
+//
+//  Created by pablo borquez on 05/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
