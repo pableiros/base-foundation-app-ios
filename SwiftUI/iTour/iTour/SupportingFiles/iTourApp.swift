9ca5796873d5f23dfc636c60f9c59355774f969e
Pablo Bórquez Urquidy <pablo.borquez.urquidy@gmail.com>
2023-11-20 17:51:21 -0700

    itour project added

---
+++ SwiftUI/iTour/iTour/SupportingFiles/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by pablo borquez on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/iTour/iTour/Templates/Destination.swift
@@ -0,0 +1,8 @@
+//
+//  Destination.swift
+//  iTour
+//
+//  Created by pablo borquez on 14/11/23.
+//
+
+import Foundation
